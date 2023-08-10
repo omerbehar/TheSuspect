48e3490544d3c6492d56b9f559f8029367aac897
omerbehar <omer.behar@gmail.com>
2023-08-10 12:06:18 +0300

    update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     public LevelDesign currentLevel;
     private const string LOADING_SCENE = "LoadingScene";
     private const string INTRO_SCENE1 = "IntroScene1";
-    private const string INTRO_SCENE2 = "IntroScene1";
+    private const string INTRO_SCENE2 = "IntroScene2";
     [SerializeField] private Transform loadingImage;
     [SerializeField] private Button nextButton;
     private void Awake()
@@ -27,18 +27,28 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
-    private void Start()
+
+    private void OnEnable()
     {
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+    }
+    private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!scene.isLoaded) return;
         if (SceneManager.GetActiveScene().name == LOADING_SCENE)
             StartCoroutine(LoadGame());
-        if (SceneManager.GetActiveScene().name == INTRO_SCENE1 || SceneManager.GetActiveScene().name == INTRO_SCENE2)
+        if (SceneManager.GetActiveScene().name is INTRO_SCENE1 or INTRO_SCENE2)
         {
+            nextButton = FindObjectOfType<Button>();
             nextButton.onClick.AddListener(OnNextButtonClicked);
         }
+        
     }
-    
-    
+
     private IEnumerator LoadGame()
     {
         //rotate loadingImage half a circle each second for x seconds then load game
@@ -47,8 +57,7 @@ public class GameManager : MonoBehaviour
             loadingImage.Rotate(0, 0, 18);
             yield return new WaitForSeconds(0.1f);
         }
-        SceneManager.LoadScene(INTRO_SCENE2);
-        nextButton.onClick.AddListener(OnNextButtonClicked);
+        SceneManager.LoadScene(INTRO_SCENE1);
     }
 
     private void OnNextButtonClicked()
