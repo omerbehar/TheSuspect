bb362228199d24564ef6093e3dba55103501d241
shsosa <37746784+shsosa@users.noreply.github.com>
2023-08-14 20:46:42 +0300

    Question interface

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -1,13 +1,28 @@
 ﻿using DefaultNamespace;
+using Screens.Interfaces;
 using UnityEngine;
 
 // This attribute makes the Question appear in the Create Asset menu
 [CreateAssetMenu(fileName = "New Question", menuName = "Quiz/Question")]
-public class Question : ScriptableObject
+public class Question : ScriptableObject, IQuestion
 {
     public string questionText;
     public string[] answers;
     public int correctAnswerIndex;
     public string clue;
     public QuestionType type;
+
+    public string QuestionText { get => questionText; set => questionText = value; }
+    public QuestionType Type { get => type; set => type = value; }
+
+    // You will implement these methods according to the type of question.
+    public void Display()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool CheckAnswer(int answerIndex)
+    {
+        throw new System.NotImplementedException();
+    }
 }
---
+++ Assets/Scripts/Screens/Interfaces/IQuestion.cs
@@ -0,0 +1,13 @@
+﻿using DefaultNamespace;
+
+namespace Screens.Interfaces
+{
+    public interface IQuestion
+    {
+        string QuestionText { get; set; }
+        QuestionType Type { get; set; }
+
+        void Display();
+        bool CheckAnswer(int answerIndex);
+    }
+}
