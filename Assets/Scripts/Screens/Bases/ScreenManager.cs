4a7884211ea2f2377f9767e4be98cf7777ed9c96
shsosa <37746784+shsosa@users.noreply.github.com>
2023-08-15 14:03:20 +0300

    UI Doc

--- Assets/Scripts/Screens/Bases/ScreenManager.cs
+++ Assets/Scripts/Screens/Bases/ScreenManager.cs
@@ -1,211 +1,224 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class ScreenManager : MonoBehaviour
+namespace Screens.Bases
 {
-    public Question[] questions;
-    private int currentQuestionIndex = 0;
-
-    private Label questionText;
-    private VisualElement answersContainer;
-    private TextField fillInTheBlankInputField;
-    private Button continueButton;
-
-    private void Awake()
+    public class ScreenManager : MonoBehaviour
     {
-        var root = GetComponent<UIDocument>().rootVisualElement;
+        public Question[] questions;
+        private int currentQuestionIndex = 0;
 
-        questionText = root.Q<Label>("questionText");
-        answersContainer = root.Q<VisualElement>("answersContainer");
-        fillInTheBlankInputField = root.Q<TextField>("fillInTheBlankInputField");
-        continueButton = root.Q<Button>("continueButton");
+        private Label questionText;
+        private VisualElement answersContainer;
+        private TextField fillInTheBlankInputField;
+        private Button continueButton;
 
-        continueButton.clicked += OnContinueButtonPressed;
-
-        DisplayQuestion(questions[currentQuestionIndex]);
-    }
-
-    public void DisplayQuestion(Question question)
-    {
-        questionText.text = question.questionText;
-
-        // Clear previous answers
-        answersContainer.Clear();
-
-        switch (question.type)
+        private void Awake()
         {
-            case QuestionType.MultipleChoice:
-                for (int i = 0; i < question.answers.Length; i++)
-                {
-                    var answerToggle = new Toggle { text = question.answers[i] };
-                    answersContainer.Add(answerToggle);
-                }
-                break;
-            
-            case QuestionType.SingleChoice:
-                for (int i = 0; i < question.answers.Length; i++)
-                {
-                    var radioButton = new RadioButton { text = question.answers[i] };
-                    answersContainer.Add(radioButton);
-                }
-                break;
-            
-            case QuestionType.FillInTheBlank:
-                fillInTheBlankInputField.visible = true;
-                break;
-            
-            case QuestionType.Sentence:
-                // Handle Sentance Type Question
-                break;
-            default:
-                throw new System.NotSupportedException($"QuestionType {question.type} is not supported.");
-        }
-    }
-    public void CheckAnswer()
-    {
-        Question currentQuestion = questions[currentQuestionIndex];
-        bool isCorrect = false;
+            var root = FindObjectOfType<UIDocument>().rootVisualElement;
 
-        switch (currentQuestion.Type)
-        {
-            case QuestionType.MultipleChoice:
-                int[] selectedAnswerIndices = GetSelectedAnswerIndices();
-                isCorrect = IsAnswerCorrect(selectedAnswerIndices, currentQuestion.correctAnswerIndices);
-                break;
+            questionText = root.Q<Label>("questionText");
+            answersContainer = root.Q<VisualElement>("answersContainer");
+            fillInTheBlankInputField = root.Q<TextField>("fillInTheBlankInputField");
+            continueButton = root.Q<Button>("continueButton");
 
-            case QuestionType.SingleChoice:
-                int singleSelectedIndex = GetSelectedAnswerIndex(); // Assuming GetSelectedAnswerIndex is designed for SingleChoice questions
-                isCorrect = IsAnswerCorrect(new int[] { singleSelectedIndex }, currentQuestion.correctAnswerIndices);
-                break;
+            continueButton.clicked += OnContinueButtonPressed;
 
-            case QuestionType.FillInTheBlank:
-                string selectedAnswerText = GetSelectedAnswerText();
-                isCorrect = selectedAnswerText.Equals(currentQuestion.answers[0]);
-                break;
-            
-            // ... similar handling for other question types ...
-
-            default:
-                throw new System.NotImplementedException($"Question type {currentQuestion.type} not supported.");
-        }
-
-        if (isCorrect)
-        {
-            Debug.Log("Correct answer!");
-        }
-        else
-        {
-            Debug.Log("Wrong answer. Try again.");
-        }
-
-        // If there are more questions, then show the next question
-        if (currentQuestionIndex < questions.Length - 1)
-        {
-            currentQuestionIndex++;
             DisplayQuestion(questions[currentQuestionIndex]);
         }
-        else
+
+        public void DisplayQuestion(Question question)
         {
-            Debug.Log("Quiz completed!");
+            questionText.text = question.questionText;
+
+            // Clear previous answers
+            answersContainer.Clear();
+
+            switch (question.type)
+            {
+                case QuestionType.MultipleChoice:
+                    for (int i = 0; i < question.answers.Length; i++)
+                    {
+                        var answerToggle = new Toggle { text = question.answers[i] };
+                        answersContainer.Add(answerToggle);
+                    }
+                    break;
+            
+                case QuestionType.SingleChoice:
+                    for (int i = 0; i < question.answers.Length; i++)
+                    {
+                        var radioButton = new RadioButton { text = question.answers[i] };
+                        answersContainer.Add(radioButton);
+                    }
+                    break;
+            
+                case QuestionType.FillInTheBlank:
+                    fillInTheBlankInputField.visible = true;
+                    break;
+            
+                case QuestionType.Sentence:
+                    // Handle Sentance Type Question
+                    break;
+                default:
+                    throw new System.NotSupportedException($"QuestionType {question.type} is not supported.");
+            }
         }
-    }
-
-    private static bool IsAnswerCorrect(int[] selectedAnswerIndices, int[] correctAnswerIndices)
-    {
-        return selectedAnswerIndices.Length == correctAnswerIndices.Length &&
-               selectedAnswerIndices.OrderBy(i => i).SequenceEqual(correctAnswerIndices.OrderBy(i => i));
-    }
-    private int[] GetSelectedAnswerIndices()
-    {
-        var indices = new List<int>();
-
-        switch (questions[currentQuestionIndex].type)
+        public void CheckAnswer()
         {
-            case QuestionType.MultipleChoice:
-                int i = 0;
-                foreach (VisualElement child in answersContainer.Children())
-                {
-                    if (child is Toggle toggle && toggle.value)
-                    {
-                        indices.Add(i);
-                    }
-                    i++;
-                }
-                break;
+            Question currentQuestion = questions[currentQuestionIndex];
+            bool isCorrect = false;
 
-            case QuestionType.SingleChoice:
-                int j = 0;
-                foreach (VisualElement child in answersContainer.Children())
-                {
-                    if (child is Toggle toggle && toggle.value)
-                    {
-                        indices.Add(j);
-                        break;
-                    }
-                    j++;
-                }
-                break;
+            switch (currentQuestion.Type)
+            {
+                case QuestionType.MultipleChoice:
+                    int[] selectedAnswerIndices = GetSelectedAnswerIndices();
+                    isCorrect = IsAnswerCorrect(selectedAnswerIndices, currentQuestion.correctAnswerIndices);
+                    break;
 
-            case QuestionType.FillInTheBlank:
-                /* For fill in the blank questions, we might not be dealing with indices, but with
+                case QuestionType.SingleChoice:
+                    int singleSelectedIndex = GetSelectedAnswerIndex(); // Assuming GetSelectedAnswerIndex is designed for SingleChoice questions
+                    isCorrect = IsAnswerCorrect(new int[] { singleSelectedIndex }, currentQuestion.correctAnswerIndices);
+                    break;
+
+                case QuestionType.FillInTheBlank:
+                    string selectedAnswerText = GetSelectedAnswerText();
+                    isCorrect = selectedAnswerText.Equals(currentQuestion.answers[0]);
+                    break;
+            
+                case QuestionType.Sentence:
+                    // Handle Sentance Type Question
+                    break;
+
+                default:
+                    throw new System.NotImplementedException($"Question type {currentQuestion.type} not supported.");
+            }
+
+            if (isCorrect)
+            {
+                Debug.Log("Correct answer!");
+            }
+            else
+            {
+                Debug.Log("Wrong answer. Try again.");
+            }
+
+            // If there are more questions, then show the next question
+            if (currentQuestionIndex < questions.Length - 1)
+            {
+                currentQuestionIndex++;
+                DisplayQuestion(questions[currentQuestionIndex]);
+            }
+            else
+            {
+                Debug.Log("Quiz completed!");
+            }
+        }
+
+        private static bool IsAnswerCorrect(int[] selectedAnswerIndices, int[] correctAnswerIndices)
+        {
+            return selectedAnswerIndices.Length == correctAnswerIndices.Length &&
+                   selectedAnswerIndices.OrderBy(i => i).SequenceEqual(correctAnswerIndices.OrderBy(i => i));
+        }
+        private int[] GetSelectedAnswerIndices()
+        {
+            var indices = new List<int>();
+
+            switch (questions[currentQuestionIndex].type)
+            {
+                case QuestionType.MultipleChoice:
+                    int i = 0;
+                    foreach (VisualElement child in answersContainer.Children())
+                    {
+                        if (child is Toggle toggle && toggle.value)
+                        {
+                            indices.Add(i);
+                        }
+                        i++;
+                    }
+                    break;
+
+                case QuestionType.SingleChoice:
+                    int j = 0;
+                    foreach (VisualElement child in answersContainer.Children())
+                    {
+                        if (child is Toggle toggle && toggle.value)
+                        {
+                            indices.Add(j);
+                            break;
+                        }
+                        j++;
+                    }
+                    break;
+
+                case QuestionType.FillInTheBlank:
+                    /* For fill in the blank questions, we might not be dealing with indices, but with
                 the actual content of an input field. This means we might need a separate method to
                 get the input field content and a different approach in the CheckAnswer method. */
-                break;
+                    break;
 
-            case QuestionType.Sentence:
-                // Similar situation as FillInTheBlank
-                break;
+                case QuestionType.Sentence:
+                    // Similar situation as FillInTheBlank
+                    break;
 
-            default:
-                throw new System.NotImplementedException($"Question type {questions[currentQuestionIndex].type} not supported.");
+                default:
+                    throw new System.NotImplementedException($"Question type {questions[currentQuestionIndex].type} not supported.");
+            }
+
+            return indices.ToArray();
         }
-
-        return indices.ToArray();
-    }
-    private int GetSelectedAnswerIndex()
-    {
-        switch (questions[currentQuestionIndex].type)
+        private int GetSelectedAnswerIndex()
         {
-            case QuestionType.MultipleChoice:
-                var toggles = answersContainer.Children().OfType<Toggle>();
-                return Array.FindIndex<>(toggles, toggle => toggle.value);
+            switch (questions[currentQuestionIndex].type)
+            {
+                case QuestionType.MultipleChoice:
+                    var toggles = answersContainer.Children().OfType<Toggle>();
+                    // Correction starts here
+                    return toggles.Select((toggle, index) => new { Index = index, Toggle = toggle })
+                        .FirstOrDefault(pair => pair.Toggle.value)
+                        ?.Index ?? -1;
+                // Correction ends here
 
-            case QuestionType.SingleChoice:
-                var radioButtons = answersContainer.Children().OfType<RadioButton>();
-                return Array.FindIndex<>(radioButtons, radioButton => radioButton.value);
+                case QuestionType.SingleChoice:
+                    var radioButtons = answersContainer.Children().OfType<RadioButton>();
+                    // Correction starts here
+                    return radioButtons.Select((radioButton, index) => new { Index = index, Radio = radioButton })
+                        .FirstOrDefault(pair => pair.Radio.value)
+                        ?.Index ?? -1;
+                // Correction ends here
 
-            case QuestionType.FillInTheBlank:
-                // For fill in the blank questions, you might need a different way to check answers
-                // For example, you could use string comparison instead of indices
-                break;
+                case QuestionType.FillInTheBlank:
+                    // For fill in the blank questions, you might need a different way to check answers
+                    // For example, you could use string comparison instead of indices
+                    break;
 
-            case QuestionType.Sentence:
-                // Handle Sentance Type Question
-                break;
+                case QuestionType.Sentence:
+                    // Handle Sentance Type Question
+                    break;
+            }
+
+            return -1;
         }
-
-        return -1;
-    }
     
-    private string GetSelectedAnswerText()
-    {
-        if (questions[currentQuestionIndex].type == QuestionType.FillInTheBlank)
+        private string GetSelectedAnswerText()
         {
-            // Assuming fillInTheBlankInputField is a TextField element in UI Toolkit
-            return fillInTheBlankInputField.value.Trim(); 
+            if (questions[currentQuestionIndex].type == QuestionType.FillInTheBlank)
+            {
+                // Assuming fillInTheBlankInputField is a TextField element in UI Toolkit
+                return fillInTheBlankInputField.value.Trim(); 
+            }
+            return string.Empty;
         }
-        return string.Empty;
-    }
-    public void OnContinueButtonPressed()
-    {
-        if (currentQuestionIndex < questions.Length - 1)
+        public void OnContinueButtonPressed()
         {
-            currentQuestionIndex++;
-            DisplayQuestion(questions[currentQuestionIndex]);
+            if (currentQuestionIndex < questions.Length - 1)
+            {
+                currentQuestionIndex++;
+                DisplayQuestion(questions[currentQuestionIndex]);
+            }
         }
     }
 }
