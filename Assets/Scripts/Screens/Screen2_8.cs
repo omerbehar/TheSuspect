821c3c90c10a92178709dcd0b196c5c140f736a1
omerbehar <omer.behar@gmail.com>
2023-08-27 15:27:45 +0300

    update

---
+++ Assets/Scripts/Screens/Screen2_8.cs
@@ -0,0 +1,50 @@
+using Screens.Bases;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Screens
+{
+    public class Screen2_8 : ScreenBase
+    {
+        [SerializeField] private Button mapButton;
+        [SerializeField] private Button carButton;
+        [SerializeField] private GameObject mapPopup;
+        [SerializeField] private GameObject carPopup;
+        [SerializeField] private Button mapCloseButton;
+        [SerializeField] private Button carCloseButton;
+        protected override void Start()
+        {
+            base.Start();
+            NextButton.interactable = true;
+            InitButtonListeners();
+        }
+
+        private void InitButtonListeners()
+        {
+            mapButton.onClick.AddListener(OnMapButtonClicked);
+            carButton.onClick.AddListener(OnCarButtonClicked);
+            mapCloseButton.onClick.AddListener(OnMapCloseButtonClicked);
+            carCloseButton.onClick.AddListener(OnCarCloseButtonClicked);
+        }
+
+        private void OnCarCloseButtonClicked()
+        {
+            carPopup.SetActive(false);
+        }
+
+        private void OnMapCloseButtonClicked()
+        {
+            mapPopup.SetActive(false);
+        }
+
+        private void OnCarButtonClicked()
+        {
+            carPopup.SetActive(true);
+        }
+
+        private void OnMapButtonClicked()
+        {
+            mapPopup.SetActive(true);
+        }
+    }
+}
