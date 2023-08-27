a72c82f9e91d71ea28a9a903f59357962b2016f7
omerbehar <omer.behar@gmail.com>
2023-08-27 15:58:47 +0300

    update

--- Assets/Scripts/Screens/Screen2_7.cs
+++ Assets/Scripts/Screens/Screen2_7.cs
@@ -11,11 +11,7 @@ namespace Screens
     public class Screen2_7 : ScreenBaseWithTimer, ILoadData, ISaveData, IHint
     {
         //[field: SerializeField] public int CorrectAnswerScore { get; set; }
-<<<<<<< Updated upstream
-        [field: SerializeField] public Button HintButton { get; set; }
-=======
         //[field: SerializeField] public Button HintButton { get; set; }
->>>>>>> Stashed changes
         [field: SerializeField] public GameObject HintPopup { get; set; }
         [field: SerializeField] public Button HintPopupCloseButton { get; set; }
         [field: SerializeField] public GameObject CorrectAnswer { get; set; }
