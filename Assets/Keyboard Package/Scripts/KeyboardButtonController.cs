d4b880c3e0a1daeeebb54ea8cbbdb8106a07493d
shsosa <37746784+shsosa@users.noreply.github.com>
2024-11-14 21:48:58 +0200

    Update KeyboardButtonController.cs

--- Assets/Keyboard Package/Scripts/KeyboardButtonController.cs
+++ Assets/Keyboard Package/Scripts/KeyboardButtonController.cs
@@ -2,49 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class KeyboardButtonController : MonoBehaviour
 {
-    [SerializeField] Image containerBorderImage;
     [SerializeField] Image containerFillImage;
-    [SerializeField] Image containerIcon;
     [SerializeField] TextMeshProUGUI containerText;
     [SerializeField] TextMeshProUGUI containerActionText;
+    [SerializeField] Image containerIcon;
 
-    private void Start() {
-        SetContainerBorderColor(ColorDataStore.GetKeyboardBorderColor());
-        SetContainerFillColor(ColorDataStore.GetKeyboardFillColor());
-        SetContainerTextColor(ColorDataStore.GetKeyboardTextColor());
-        SetContainerActionTextColor(ColorDataStore.GetKeyboardActionTextColor());
+    public Color pressedColor = Color.gray;
+    private Color originalFillColor;
+
+    private void Start()
+    {
+        originalFillColor = containerFillImage.color;
+
+        // Add event trigger
+        EventTrigger trigger = gameObject.AddComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerDown;
+        entry.callback.AddListener((data) => { OnPress(); });
+        trigger.triggers.Add(entry);
+
+        entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerUp;
+        entry.callback.AddListener((data) => { OnRelease(); });
+        trigger.triggers.Add(entry);
     }
 
-    public void SetContainerBorderColor(Color color) => containerBorderImage.color = color;
+    public void OnPress()
+    {
+        containerFillImage.color = pressedColor;
+    }
+
+    public void OnRelease()
+    {
+        containerFillImage.color = originalFillColor;
+    }
+
     public void SetContainerFillColor(Color color) => containerFillImage.color = color;
     public void SetContainerTextColor(Color color) => containerText.color = color;
-    public void SetContainerActionTextColor(Color color) { 
+    public void SetContainerActionTextColor(Color color)
+    {
         containerActionText.color = color;
         containerIcon.color = color;
     }
 
-    public void AddLetter() {
-        if(GameManagerKB.Instance != null) {
+    public void AddLetter()
+    {
+        if (GameManagerKB.Instance != null)
+        {
             GameManagerKB.Instance.AddLetter(containerText.text);
-        } else {
+        }
+        else
+        {
             Debug.Log(containerText.text + " is pressed");
         }
     }
-    public void DeleteLetter() { 
-        if(GameManagerKB.Instance != null) {
+
+    public void DeleteLetter()
+    {
+        if (GameManagerKB.Instance != null)
+        {
             GameManagerKB.Instance.DeleteLetter();
-        } else {
+        }
+        else
+        {
             Debug.Log("Last char deleted");
         }
     }
-    public void SubmitWord() {
-        if(GameManagerKB.Instance != null) {
+
+    public void SubmitWord()
+    {
+        if (GameManagerKB.Instance != null)
+        {
             GameManagerKB.Instance.SubmitWord();
-        } else {
+        }
+        else
+        {
             Debug.Log("Submitted successfully!");
         }
     }
